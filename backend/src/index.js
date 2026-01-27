4130c0c8ea2c7ca7c83a2a166d6f0e0b414eec06
Ashutosh Kumar <ashutosh2003.sahi@gmail.com>
2026-01-27 09:30:59 +0530

    fixed minor issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,12 @@ import { app } from "./app.js";
 
 connectDB()
   .then(() => {
-    console.log("MONGODB connected successfuly!!");
-    server.listen(process.env.PORT || 8000, () => {
+    console.log("MONGODB connected successfully!!");
+    app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on the port ${process.env.PORT}`);
     });
     app.on("error", (error) => {
-      console.log("ERRR", error);
+      console.log("ERROR", error);
       throw error;
     });
   })
